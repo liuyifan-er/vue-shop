096a4766e8b4e4c68c3e504e9e6cc7472f527c9e
liuyifan-er <liuyifan_1998er@126.com>
2020-07-25 18:49:03 +0800

    完成商品分类

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import router from './router'
 import './plugins/element.js'
 // 导入全局样式表
 import './assets/css/global.css'
+import TreeTable from 'vue-table-with-tree-grid'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
+// import { Tree } from 'element-ui'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/V1/'
 axios.interceptors.request.use(config => {
@@ -15,7 +17,7 @@ axios.interceptors.request.use(config => {
 })
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
-
+Vue.component('tree-table', TreeTable)
 new Vue({
   router,
   render: h => h(App)
